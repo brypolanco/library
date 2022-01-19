e051774e1329a2b4d858b16ea827fd89271d29c4
brypolanco <brpolanc@gmail.com>
2022-01-19 18:51:28 -0500

    Read button changes read status

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ function addBookToLibrary() {
   let pages = window.prompt('Enter number of pages');
   let read = window.prompt('Have you read the book? yes or no.');
 
-  const aBook = new Book(title, author, pages, read);
+  let aBook = new Book(title, author, pages, read);
   myLibrary.push(aBook);
 
   console.log(myLibrary);
 }
 
 //Placeholder books
-const Book1 = new Book('My Hero Academia','Kōhei Horikoshi',212,'No');
-const Book2 = new Book('The Great Gatsby','F. Scott Fitzgerald',208,'Yes');
+let Book1 = new Book('My Hero Academia','Kōhei Horikoshi',212,'No');
+let Book2 = new Book('The Great Gatsby','F. Scott Fitzgerald',208,'Yes');
 myLibrary.push(Book1);
 myLibrary.push(Book2);
 
@@ -40,50 +40,57 @@ function loopArray(){
     });
 }
 
+//Add DOM elements
 function addBookToDOM(addBook, arrayNum){
     let bookDisplay = document.createElement('div');
     bookDisplay.textContent = addBook.info;
     bookContainer.appendChild(bookDisplay).className = `book`;
+    readBook(bookDisplay, arrayNum);
     removeBook(bookDisplay, arrayNum);
-    readBook(bookDisplay, arrayNum, addBook);
 }
 
 function removeBook(rmvBook, arrayNum){
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
     rmvBook.appendChild(removeButton).className = 'remove-book';
-    removeButton.addEventListener('click',()=>{
-        rmvBook.textContent = null;
-        myLibrary.splice(arrayNum,1);
-        console.log(myLibrary);
-    });
-
 }
 
-function readBook(rdBook, arrayNum, bookObj){
+function readBook(rdBook, arrayNum){
     const readButton = document.createElement('button');
     readButton.textContent = 'Read';
-    rdBook.appendChild(readButton).id='read-button';
-    readButton.addEventListener('click',()=>{
-        if(bookObj.read==='Yes'){
-            bookObj.read = 'No';
-            rdBook.textContent = bookObj.info;
-        }
-        else if(bookObj.read==='No'){
-            bookObj.read = 'Yes';
-            rdBook.textContent = bookObj.info;
-        }
-        else{
-            alert('Delete book and put yes or no if you read the book');
-        }
-        console.log(rdBook);
-    });
+    rdBook.appendChild(readButton).className='read-book';
 }
 
 const newButton = document.createElement('button');
 newButton.textContent = 'New Book';
 newButton.id = 'new-book'
 document.body.prepend(newButton);
+
+//Event Listeners
+let removeBtnArray = Array.from(document.querySelectorAll('.remove-book'));
+removeBtnArray.forEach((btn, index) => btn.addEventListener('click',()=>{
+    btn.parentElement.textContent = null;
+    myLibrary.splice(index,1);
+    console.log(myLibrary);
+}));
+
+let readBtnArray = Array.from(document.querySelectorAll('.read-book'));
+readBtnArray.forEach((btn, index) => btn.addEventListener('click',()=>{
+    if(myLibrary[index].read==='Yes'){
+        myLibrary[index].read = 'No';
+        myLibrary[index].info = `${myLibrary[index].title} by ${myLibrary[index].author}, ${myLibrary[index].pages} pages, Read: ${myLibrary[index].read}`;
+        btn.parentElement.textContent = myLibrary[index].info;
+    }
+    else if(myLibrary[index].read==='No'){
+        myLibrary[index].read = 'Yes';
+        myLibrary[index].info = `${myLibrary[index].title} by ${myLibrary[index].author}, ${myLibrary[index].pages} pages, Read: ${myLibrary[index].read}`;
+        btn.parentElement.textContent = myLibrary[index].info;
+    }
+    else{
+        alert('Delete book and put yes or no if you read the book');
+    }
+    console.table(myLibrary);
+}));
 
 newButton.addEventListener('click', ()=>{
     addBookToLibrary();
