3b43c916e2b04c3596546a10e2c161c4d1c8b0ed
brypolanco <brpolanc@gmail.com>
2022-01-18 22:12:14 -0500

    Append each book to DOM

--- script.js
+++ script.js
@@ -5,15 +5,42 @@ function Book(title, author, pages, read){
     this.author = author,
     this.pages = pages,
     this.read = read,
-    this.info = `${title} by ${author}, ${pages} pages, ${read}`
+    this.info = `${title} by ${author}, ${pages} pages, Read: ${read}`
 }
 
 
-function addBookToLibrary(objName) {
+function addBookToLibrary() {
   let title = window.prompt('Enter title of book');
   let author = window.prompt('Enter author');
   let pages = window.prompt('Enter number of pages');
   let read = window.prompt('Have you read the book? yes or no.');
 
-  let objName = new Book(title, author, pages, read);
+  const aBook = new Book(title, author, pages, read);
+  myLibrary.push(aBook);
+
+  console.log(aBook);
+}
+
+//Placeholder books
+const Book1 = new Book('My Hero Academia','Kōhei Horikoshi',212,'No');
+const Book2 = new Book('The Great Gatsby','F. Scott Fitzgerald',208,'Yes');
+myLibrary.push(Book1);
+myLibrary.push(Book2);
+
+addBookToLibrary();
+
+const bookContainer = document.createElement('div');
+bookContainer.id = 'book-container';
+document.body.appendChild(bookContainer);
+
+loopArray();
+
+function loopArray(){
+    myLibrary.forEach(book=>{
+        let i = 0;
+        let bookDisplay = document.createElement('div');
+        bookDisplay.textContent = book.info;
+        bookContainer.appendChild(bookDisplay).id = `book${i}`;
+        i++
+    });
 }
