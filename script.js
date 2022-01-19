ccd88463c8f860a5fdcbbf0a5e1eb0f21c956b2c
brypolanco <brpolanc@gmail.com>
2022-01-18 19:01:50 -0500

    Create a constructor for the books and function to make books

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let myLibrary = [];
 
-function Book() {
-  
+function Book(title, author, pages, read){
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read,
+    this.info = `${title} by ${author}, ${pages} pages, ${read}`
 }
 
-function addBookToLibrary() {
-  
+
+function addBookToLibrary(objName) {
+  let title = window.prompt('Enter title of book');
+  let author = window.prompt('Enter author');
+  let pages = window.prompt('Enter number of pages');
+  let read = window.prompt('Have you read the book? yes or no.');
+
+  let objName = new Book(title, author, pages, read);
 }
